7a54e48fbb6d61a35eb43cbbfbb27c56535d363e
Anastasiya <anastasiyazavialova@gmail.com>
2024-10-31 18:10:01 +0600

    Update GreyActivity.kt

--- app/src/main/java/com/yt/greenarchitectapp/screens/activities/GreyActivity.kt
+++ app/src/main/java/com/yt/greenarchitectapp/screens/activities/GreyActivity.kt
@@ -3,15 +3,30 @@ package com.yt.greenarchitectapp.screens.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.yt.greenarchitectapp.R
 
 class GreyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Surface {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.regfon),
+                        contentDescription = "Background Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
                 Text(text = "Страничка Настасьи")
             }
         }
