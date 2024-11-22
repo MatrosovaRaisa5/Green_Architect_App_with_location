f95237867d7399d39ad2e20b1f0e0b4e1bbbbe8e
Raisa Matrosova <matrosova-05@list.ru>
2024-11-22 22:11:12 +0600

    Commit Message

--- app/src/main/java/com/yt/greenarchitectapp/screens/bottomNavigation/screens/functions/AgrosovetActivity.kt
+++ app/src/main/java/com/yt/greenarchitectapp/screens/bottomNavigation/screens/functions/AgrosovetActivity.kt
@@ -4,23 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yt.greenarchitectapp.R
+import com.yt.greenarchitectapp.commonui.CommonButton
+import com.yt.greenarchitectapp.commonui.Text17_600
 import com.yt.greenarchitectapp.commonui.Text22_700
 import com.yt.greenarchitectapp.commonui.Text34_700
+import com.yt.greenarchitectapp.ui.theme.orange
 
 class AgrosovetActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,23 +45,93 @@ class AgrosovetActivity : ComponentActivity() {
                     contentScale = ContentScale.Crop,
                     modifier = Modifier.fillMaxSize()
                 )
-            }
 
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                item {
-                    Text34_700(
-                        text = "Агросовет",
-                        color = Color.Black,
-                        modifier = Modifier
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
+                LazyColumn(
+                    modifier = Modifier.padding(10.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+
+                    item {
+                        Row {
+                            Text34_700(
+                                text = "Агросовет",
+                                color = Color.Black,
+                                modifier = Modifier.padding(top=20.dp,start=20.dp,end = 10.dp)
+                            )
+                            Icon(
+                                painter = painterResource(id = R.drawable.asovet),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(70.dp)
+                                .padding(top=20.dp),
+                                tint = Color.Unspecified,
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text17_600(
+                            text = "Ваш ключ к успешному урожаю:",
+                            color = orange,
+                            modifier = Modifier.padding(top=20.dp, start=28.dp)
+                        )
+                        Text17_600(
+                            text = "Получите профессиональные советы агронома",
+                            color = orange,
+                            modifier = Modifier.padding(top=20.dp, start=28.dp)
+                        )
+                    }
+                    item {
+                        Text(
+                            text = "Специалисты",
+                            fontSize = 20.sp,
+                            fontFamily = FontFamily.Default,
+                            fontWeight = FontWeight.W600,
+                            color = Color(0xFF29852E),
+                            modifier = Modifier.padding(top = 20.dp, start = 28.dp)
+                        )
+                    }
+                    item { AgronomistCard(R.drawable.agronom1, "Ольга Васильева",
+                        "Приветствую всех! Я агроном с 11-летним стажем и настоящая любительница цветов. Моя страсть — это розы! Я занимаюсь их разведением и знаю, как сделать так, чтобы они радовали вас своим цветением. Если вы хотите научиться ухаживать за цветами или выбрать подходящие сорта для вашего сада, я с удовольствием помогу вам!",
+                        288, 456)
+                    }
+                    item { AgronomistCard(R.drawable.agronom2, "Сергей Иванов",
+                        "Здравствуйте, друзья! Я агроном с 9-летним опытом, и моя любовь — это томаты! Я разводил множество сортов, включая знаменитые бычье сердце, воловье сердце, кенигсберг и другие. Знаю, как вырастить вкусные и сочные помидоры, и готов поделиться секретами их ухода. Если вы хотите получить отличный урожай, обращайтесь — помогу вам выбрать лучшие сорта и научу, как за ними ухаживать!",
+                        149, 576)
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun AgronomistCard(imageResId: Int, name: String, description: String, articles: Int, answers: Int) {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color.White,
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Image(
+                painter = painterResource(id = imageResId),
+                contentDescription = name,
+                modifier = Modifier.size(160.dp).align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(9.dp))
+            Text22_700(text = name, color = Color.Black)
+            Spacer(modifier = Modifier.height(7.dp))
+            Text17_600(text = description, color = Color.Gray)
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                Text17_600(text = "Статьи: $articles", color = Color.Black)
+                Text17_600(text = "Ответы: $answers", color = Color.Black)
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            CommonButton(
+                text = "Написать",
+                backgroundColor = orange,
+                foregroundColor = Color.White,
+                onClick = {  }
+            )
         }
     }
 }
--- app/src/main/java/com/yt/greenarchitectapp/screens/bottomNavigation/screens/functions/LibraryActivity.kt
+++ app/src/main/java/com/yt/greenarchitectapp/screens/bottomNavigation/screens/functions/LibraryActivity.kt
@@ -4,30 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yt.greenarchitectapp.R
-import com.yt.greenarchitectapp.commonui.CommonButton
-import com.yt.greenarchitectapp.commonui.CommonHeader
-import com.yt.greenarchitectapp.commonui.Text22_700
-import com.yt.greenarchitectapp.commonui.Text28_600
+import com.yt.greenarchitectapp.commonui.CommonSearchBar
+import com.yt.greenarchitectapp.commonui.Text17_600
 import com.yt.greenarchitectapp.commonui.Text34_700
-import com.yt.greenarchitectapp.commonui.Text67_800
+import com.yt.greenarchitectapp.commonui.Text65_800
 import com.yt.greenarchitectapp.ui.theme.orange
 
 class LibraryActivity : ComponentActivity() {
@@ -41,6 +43,7 @@ class LibraryActivity : ComponentActivity() {
     @Composable
     fun Content() {
         Surface {
+            val search = remember { mutableStateOf("") }
             Box(modifier = Modifier.fillMaxSize()) {
                 Image(
                     painter = painterResource(id = R.drawable.catfon),
@@ -57,14 +60,101 @@ class LibraryActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
                 item {
-                    Text34_700(
-                        text = "Статьи",
-                        color = Color.Black,
-                        modifier = Modifier.padding(top=20.dp,start = 28.dp)
-                    )
+                    Row {
+                        Text34_700(
+                            text = "Библиосад",
+                            color = Color.Black,
+                            modifier = Modifier.padding(top=20.dp,start=20.dp,end = 10.dp)
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.books),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(60.dp)
+                                .padding(top=20.dp),
+                            tint = Color.Unspecified,
+                        )
+                    }
                     Spacer(modifier = Modifier.height(10.dp))
+                    Text17_600(
+                        text = "Интересно о садоводстве с нами!",
+                        color = orange,
+                        modifier = Modifier.padding(top = 20.dp, start = 28.dp)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                item {
+                    CommonSearchBar(
+                        text = search,
+                        isEnabled = true,
+                        modifier = Modifier.padding(horizontal = 20.dp)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+
+                item {
+                    ArticleCard(
+                        "Домашние растения: Узнайте, кто на самом деле правит вашим домом!",
+                        R.drawable.picture1
+                    )
+                }
+                item {
+                    ArticleCard(
+                        "Садоводство для начинающих: Как вырастить свой огород, не сойдя с ума?",
+                        R.drawable.picture2
+                    )
                 }
             }
         }
     }
 }
+
+    @Composable
+    fun ArticleCard(title: String, imageRes: Int) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(10.dp)
+                .clip(RoundedCornerShape(20.dp))
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.4f))
+                    .clip(RoundedCornerShape(30.dp))
+            )
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(
+                    text = "Читать статью >",
+                    color = Color.White,
+                    modifier = Modifier.padding(top = 20.dp, end = 20.dp),
+                    textAlign = TextAlign.End,
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.W600,
+                )
+                Text(
+                    text = title,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 20.dp, end = 20.dp, bottom = 16.dp),
+                    textAlign = TextAlign.End,
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Default,
+                    fontWeight = FontWeight.W600,
+                )
+            }
+        }
+    }
